631ac50a77438ce0fb60461a1090ea04415f022b
Victory Olamide Oyekunle <76197526+VickyKunmi@users.noreply.github.com>
2023-06-23 01:07:22 +0000

    Add files via upload

--- redux/cartSlice.js
+++ redux/cartSlice.js
@@ -2,12 +2,6 @@ import { getDistance } from "geolib";
 
 const { createSlice } = require("@reduxjs/toolkit");
 
-// const initialState = {
-//   drugs: [],
-//   quantity: 0,
-//   deliveryFee: 0,
-//   total: 0,
-// };
 
 const loadCartStateFromLocalStorage = () => {
   try {
@@ -36,47 +30,22 @@ const cartSlice = createSlice({
     drugs: [],
     quantity: 0,
     deliveryFee: 0,
+    totalFee: 0,
     total: 0,
   },
   reducers: {
     addDrug: (state, action) => {
-      // const drug = action.payload;
-
-      // const existingDrug = state.drugs.find((d) => d.id === drug.id);
-
-      // if (existingDrug) {
-      //   // If the drug already exists in the cart, update its quantity
-      //   existingDrug.quantity += drug.quantity;
-      // } else {
-      //   // Otherwise, add the drug to the cart
-      //   state.drugs.push({ ...drug }); // Push a new object with the spread operator
-      // }
-
-      // state.quantity += drug.quantity;
-      // state.total += drug.price * drug.quantity;
-      // state.totalFee = state.total;
-
-      // saveCartStateToLocalStorage(state);
+      
       state.drugs.push(action.payload);
       state.quantity += 1;
       state.total += action.payload.price * action.payload.quantity;
       state.totalFee +=
         action.payload.price * action.payload.quantity + state.deliveryFee;
-    },
+    
+    
+      },
     removeDrug: (state, action) => {
-      // const drugId = action.payload;
-
-      // const index = state.drugs.findIndex((d) => d.id === drugId);
-
-      // if (index !== -1) {
-      //   const removedDrug = state.drugs.splice(index, 1)[0];
-
-      //   state.quantity -= removedDrug.quantity;
-      //   state.total -= removedDrug.price * removedDrug.quantity;
-      //   state.totalFee = state.total;
-
-        // Save the updated cart state to local storage
-        // saveCartStateToLocalStorage(state);
+      
         const drugId = action.payload;
         const drugIndex = state.drugs.findIndex((drug) => drug.id === drugId);
         if (drugIndex !== -1) {
@@ -85,20 +54,18 @@ const cartSlice = createSlice({
           state.total -= removedDrug.price * removedDrug.quantity;
           state.totalFee -=
             removedDrug.price * removedDrug.quantity + state.deliveryFee;
+        
         }
-      }
-    },
-    updateDeliveryFee: (state, action) => {
-      //   state.deliveryFee = action.payload;
-      const { deliveryAddress, sourceLocation } = action.payload;
-
-      // Calculate the distance between delivery address and source location
-      const distance = getDistance(sourceLocation, deliveryAddress);
-
-      // Assuming delivery fee is calculated based on distance
-      // You can modify this calculation according to your requirements
-      const deliveryFee = distance * 0.1; // Assuming delivery fee is 0.1 unit per distance unit
-      state.deliveryFee = deliveryFee;
+      },
+      updateDeliveryFee: (state, action) => {
+        const deliveryFee = action.payload;
+        state.deliveryFee = deliveryFee;
+        state.totalFee = state.total + state.deliveryFee;
+      },
+      updateTotalFee: (state, action) => {
+        const totalFee = action.payload;
+        state.totalFee = totalFee;
+      },
     },
     // reset: (state) => {
     reset: () => {
@@ -109,54 +76,8 @@ const cartSlice = createSlice({
   },
 );
 
-export const { addDrug, reset, removeDrug, updateDeliveryFee } =
+export const { addDrug, reset, removeDrug, updateDeliveryFee, updateTotalFee } =
   cartSlice.actions;
 export default cartSlice.reducer;
 
-// reducers.js
 
-// import { createReducer } from "@reduxjs/toolkit";
-// import { addDrug, removeDrug } from "./action";
-
-// const initialState = {
-//   drugs: [],
-//   quantity: 0,
-//   total: 0,
-//   totalFee: 0,
-// };
-
-// const cartReducer = createReducer(initialState, (builder) => {
-//   builder
-//     .addCase(addDrug, (state, action) => {
-//       const drug = action.payload;
-
-//       const existingDrug = state.drugs.find((d) => d.id === drug.id);
-
-//       if (existingDrug) {
-//         // If the drug already exists in the cart, update its quantity
-//         existingDrug.quantity += drug.quantity;
-//       } else {
-//         // Otherwise, add the drug to the cart
-//         state.drugs.push(drug);
-//       }
-
-//       state.quantity += drug.quantity;
-//       state.total += drug.price * drug.quantity;
-//       state.totalFee = state.total;
-//     })
-//     .addCase(removeDrug, (state, action) => {
-//       const drugId = action.payload;
-
-//       const index = state.drugs.findIndex((d) => d.id === drugId);
-
-//       if (index !== -1) {
-//         const removedDrug = state.drugs.splice(index, 1)[0];
-
-//         state.quantity -= removedDrug.quantity;
-//         state.total -= removedDrug.price * removedDrug.quantity;
-//         state.totalFee = state.total;
-//       }
-//     });
-// });
-
-// export default cartReducer;
